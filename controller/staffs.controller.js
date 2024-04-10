5ca35ddc8a37eecb99b64742c1ee410ce5c690a5
zink-ndp <phucvinhlp02@gmail.com>
2024-04-10 15:37:41 +0700

    fix staff invoice

--- controller/staffs.controller.js
+++ controller/staffs.controller.js
@@ -54,7 +54,7 @@ const staffsController = {
     try {
       const { id } = req.params;
       const [rows, fields] = await pool.query(
-        "select i.*, c.CTM_NAME, s.STF_NAME from invoice i join customer c on c.CTM_ID = i.CTM_ID join staff s on s.STF_ID=i.STF_ID left join voucher v on v.VOU_ID=i.VOU_ID where s.STF_ID=?",
+        "select i.*, v.*, a.*, c.*, s.STF_NAME from invoice i join staff s on s.STF_ID=i.STF_ID join customer c on c.CTM_ID=i.CTM_ID left join voucher v on v.VOU_ID=i.VOU_ID left join address a on a.ADR_ID=i.ADR_ID where s.STF_ID=? order by (STR_TO_DATE(SUBSTRING_INDEX(INV_CREATEDAT, '-', -1) , '%d/%m/%Y')) desc",
         [id]
       );
       res.json({
